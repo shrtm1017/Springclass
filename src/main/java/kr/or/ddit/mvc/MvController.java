a3c03bbeafdf26444f67ea5984d57f2881a7a594
dohyun <dohyun.shrtm1015@gmail.com>
2019-03-06 14:39:42 +0900

    spring mvc validator, 예외처리, interceptor
    validator
    1.validator Interface 구현
    2.jsr303 (hibernate validator)
    3.1+2
    
    예외처리
    1.@ExceptionHandler(컨트롤러 마다..)
    2.@ControllerAdvice(모든 컨트롤러에 적용)
    3.@ResponseStatus(커스텀 예외가 발생할 경우 지정한 상태코드로 처리 )
    
    인터셉터
    1.profileingInterceptor
    
    *error 페이지 이관(jsp/servlet)

--- src/main/java/kr/or/ddit/mvc/MvController.java
+++ src/main/java/kr/or/ddit/mvc/MvController.java
@@ -4,15 +4,26 @@ import java.io.File;
 import java.io.IOException;
 import java.util.UUID;
 
-import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.multipart.MultipartFile;
+
+import kr.or.ddit.exception.NoFileException;
+import kr.or.ddit.user.model.UserVo;
+import kr.or.ddit.user.validator.UserVoValidator;
 
 @Controller
 public class MvController {
@@ -47,6 +58,79 @@ public class MvController {
 			e.printStackTrace();
 		}
 		return "/mvc/view";
+	}
+	@RequestMapping("/textView")
+	public String textView(){
+		return "/mvc/textView";
+		
+		
+	}
+	//@@RequestParam 어노테이션을 적용하지 않아도
+	//인스턴스명이랑 동일하면 바인딩
+	//파라미터명 이랑 인스턴스명이랑 다를 경우 -->@@RequestParam
+	
+	//BindingResult 객체는 command객체(vo)에 바인딩 과정에서 발생한 결과를 담는
+	//객체로 반드시 command객체 메소드 인자 뒤에 위치해야한다.
+	//UserVo userVo,BindingResult result ,Model model :O
+	//UserVo userVo,Model model,BindingResult result  :x 
+	@RequestMapping("/textReq")
+	public String textReq(UserVo userVo,BindingResult result ,Model model){
+		//UserVoValidator 생성해줘야 실행함
+		new UserVoValidator().validate(userVo, result);
+		if(result.hasErrors()){
+			logger.debug("has error");
+			return "/mvc/textView";
+		}
+		logger.debug("userId"+userVo.getUserId());
+		logger.debug("pass"+userVo.getPass());
+		
+		//pass : 8자리 이상
+//		if(pass.length() <8){
+//			model.addAttribute("passwordLengthMsg","비밀번호 8 자리 이상이여야합니다.");
+//			
+//		}
+		return "/mvc/textView";
+	}
+	@RequestMapping("/textReqJsr303")
+	public String textReqJsr303(@Valid UserVo userVo,BindingResult result){
+		logger.debug("result.hasErrors :"+result.hasErrors());
+		return "/mvc/textView";
+	}
+	@RequestMapping("/textReqValJsr303")
+	public String textReqValJsr303(@Valid UserVo userVo,BindingResult result){
+		logger.debug("textReqValJsr:"+result.hasErrors());
+		return "/mvc/textView";
+	}
+	@InitBinder
+	public void initBinder(WebDataBinder binder){
+		binder.addValidators(new UserVoValidator());
+	}
+	/**
+	* Method : throwArithmeticException
+	* 작성자 : PC03
+	* 변경이력 :
+	* @return
+	* Method 설명 :Arithmetic 예외 강제 발생
+	*/
+	@RequestMapping("/throwArith")
+	public String throwArithmeticException(){
+		
+		if(1==1)
+		throw new ArithmeticException();
+		return "/mvc/textView";
+	}
+	
+	@RequestMapping("/throwNoFileException")
+	public String throwNoFileException() throws NoFileException{
+		Resource resource =new ClassPathResource("kr/or/ddit/config/sptring/no-ex.xml");
+		
+		try {
+			resource.getFile();
+		} catch (IOException e) {
+			e.printStackTrace();
+			throw new NoFileException();
+		}
+		return "/mvc/textView";
 	}
 
 }
--- src/main/java/kr/or/ddit/user/model/UserVo.java
+++ src/main/java/kr/or/ddit/user/model/UserVo.java
@@ -6,19 +6,28 @@ import java.util.Date;
 import javax.servlet.http.HttpSession;
 import javax.servlet.http.HttpSessionBindingEvent;
 import javax.servlet.http.HttpSessionBindingListener;
+import javax.validation.constraints.Size;
 
+import org.hibernate.validator.constraints.NotBlank;
+import org.hibernate.validator.constraints.NotEmpty;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class UserVo implements HttpSessionBindingListener{
+	//@NotBlank//white space 가능
+	//white space도 거절
+	//에러코드 : 어노테이션 명 --> 메세지 소스에 어노테이션명.필드 = 에러메시지 등록
 	private Logger logger = LoggerFactory.getLogger(UserVo.class);
+//	@NotEmpty
 	private String userId; // 사용자 아이디
+	
 	private String userNm; // 사용자 이름
 	private String alias; // 별명
 	private String addr1; // 주소
 	private String addr2; // 상세주소
 	private String zipcode; // 상세주소
 	private Date reg_dt; // 사용자 비밀번호
+//	@Size(min=8)
 	private String pass;// 사용자 비밀번호
 	private String filename;
 	private String realFilename;
